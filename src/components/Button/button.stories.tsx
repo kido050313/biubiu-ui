b9089b58ae61e0d8f1d0ab3cfbb4f7c61dcede8a
kido <1059064141@qq.com>
2020-09-03 23:09:48 +0800

    feat: 修改storybook main.js文件，添加button组件stories.tsx文件

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-
-import { Button, ButtonProps } from './Button';
+import Button from './button';
+// import ButtonProps from './button';
+// import { ButtonProps } from '../../stories/Button';
 
 export default {
-  title: 'Example/Button按钮',
-  component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+    title: 'BiubiuUi/Button自定义按钮',
+    component: Button,
 } as Meta;
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+const Template: Story = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
@@ -37,4 +36,3 @@ Small.args = {
   size: 'small',
   label: 'Button',
 };
-
